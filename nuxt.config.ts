ed6bf6f3078df40d7138bee208dad5cd6715e57a
imlargo <jlargob@unal.edu.co>
2026-04-06 08:33:33 -0500

    feat: integrate Tailwind CSS with Nuxt.js
    - Added Tailwind CSS and its Vite plugin to the project dependencies.
    - Configured Nuxt.js to use Tailwind CSS by importing it in the main configuration file.
    - Created a new CSS file to include Tailwind's base styles.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
@@ -9,5 +11,12 @@ export default defineNuxtConfig({
     '@nuxtjs/color-mode',
     '@nuxtjs/eslint-module',
     '@nuxtjs/i18n'
-  ]
+  ],
+
+  css: ['~/assets/css/tailwind.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 })
