b7eef9a8996b4e1e10c1ed1ef82e2ab43051d0dd
imlargo <jlargob@unal.edu.co>
2026-06-01 10:47:40 -0500

    feat: update ESLint configuration to allow console statements in logger helper

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,14 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 import eslintConfigPrettier from 'eslint-config-prettier/flat'
 
 export default withNuxt(
-  // Your custom configs here
   eslintConfigPrettier,
+  {
+    rules: {
+      'no-console': ['error', { allow: [] }],
+    },
+  },
+  {
+    files: ['app/lib/helpers/logger.ts'],
+    rules: { 'no-console': 'off' },
+  },
 )
