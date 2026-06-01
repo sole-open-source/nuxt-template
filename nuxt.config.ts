18a324dce02169b8915a6a555a406e59cc31f8be
imlargo <jlargob@unal.edu.co>
2026-06-01 10:53:16 -0500

    feat: add Vitest configuration for testing with Nuxt environment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 import tailwindcss from '@tailwindcss/vite'
 
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
@@ -11,18 +10,15 @@ export default defineNuxtConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   shadcn: {
-    /**
-     * Prefix for all the imported component.
-     * @default "Ui"
-     */
     prefix: '',
-    /**
-     * Directory that the component lives in.
-     * Will respect the Nuxt aliases.
-     * @link https://nuxt.com/docs/api/nuxt-config#alias
-     * @default "@/components/ui"
-     */
     componentDir: '@/components/ui',
+  },
+
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.API_BASE_URL ?? '',
+    },
   },
 })
