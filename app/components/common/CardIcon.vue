18ebb5fbf53e4a7b1e8b77706f7c132fb11879ca
imlargo <jlargob@unal.edu.co>
2026-07-06 11:39:46 -0500

    feat: add AsyncView, CardIcon, EmptyState, and PageHeader components; remove index.ts

---
+++ app/components/common/CardIcon.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+  description: string
+}>()
+</script>
+
+<template>
+  <Card>
+    <CardHeader>
+      <slot name="icon" />
+      <CardTitle>{{ title }}</CardTitle>
+      <CardDescription>{{ description }}</CardDescription>
+    </CardHeader>
+    <slot />
+  </Card>
+</template>
