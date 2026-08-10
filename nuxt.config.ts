fef8c3daf8fe93afba11411b204363b3e6366497
imlargo <jlargob@unal.edu.co>
2026-08-10 14:56:55 -0500

    feat: add @vueuse/nuxt integration and update dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', 'shadcn-nuxt'],
+  modules: ['@nuxt/eslint', 'shadcn-nuxt', '@vueuse/nuxt'],
   css: ['~/assets/css/tailwind.css'],
   vite: {
     plugins: [tailwindcss()],
