c2c661208dd3533ebba9afd47d452be717fe6370
imlargo <jlargob@unal.edu.co>
2026-08-10 14:37:58 -0500

    chore: add ESLint and Prettier configuration
    - Updated nuxt.config.ts to include @nuxt/eslint module.
    - Modified package.json to add linting and formatting scripts, and added necessary dependencies for ESLint and Prettier.
    - Created a pre-commit hook using Husky to run lint-staged.
    - Added .prettierignore to exclude certain files and directories from formatting.
    - Created .prettierrc for Prettier configuration.
    - Added eslint.config.mjs to integrate ESLint with Prettier.

---
+++ eslint.config.mjs
@@ -0,0 +1,7 @@
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs'
+import prettier from 'eslint-config-prettier'
+
+export default withNuxt(prettier, {
+  ignores: ['template/**', 'example/**'],
+})
