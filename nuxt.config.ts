16ae16df79dc6efaec1f1ba57334b0cb41b9e2dd
imlargo <jlargob@unal.edu.co>
2026-08-10 15:02:37 -0500

    feat: add Pinia integration and update dependencies in package.json and nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', 'shadcn-nuxt', '@vueuse/nuxt'],
+  modules: ['@nuxt/eslint', 'shadcn-nuxt', '@vueuse/nuxt', '@pinia/nuxt'],
   css: ['~/assets/css/tailwind.css'],
   vite: {
     plugins: [tailwindcss()],
